0e1dc78fc7453ffcfa8983bda2cb298fdad1ebae
Leong PH <leong@mail.com>
2021-01-31 17:30:20 +0800

    Initial Commit

---
+++ TestC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TestC
+//
+//  Created by Leong Ping How on 31/01/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
